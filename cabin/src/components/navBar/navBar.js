11fa66741c1fc77f0ed26d66ae2d84fe27e4389a
Bob Ross <ministash14@gmail.com>
2018-12-09 21:07:08 -0700

    Technically finished the main page, outside of providing links and proper images

--- cabin/src/components/navBar/navBar.js
+++ cabin/src/components/navBar/navBar.js
@@ -10,10 +10,10 @@ class navBar extends React.Component {
                 <div></div>
                 <img width={80} height={45} brand="logo" className="navImg" alt="Logo" src={logo} />
                 <div></div>
-                <div className="navLink">Menu</div>
-                <div className="navLink">About Us</div>
-                <div className="navLink">Location</div>
-                <div className="navLink">Contact</div>
+                <div className="navLink1 navLink">Menu</div>
+                <div className="navLink2 navLink">About Us</div>
+                <div className="navLink3 navLink">Location</div>
+                <div className="navLink4 navLink">Contact</div>
             </div>
         )
     }
